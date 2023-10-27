5dc8985ad42ceb5023ff789fe03b19587c50f2db
hostdog01 <hostdog01@naver.com>
2023-10-27 01:19:21 +0000

    tt

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   const codeValue = process.env.CODE;
   
   if (codeValue) {
